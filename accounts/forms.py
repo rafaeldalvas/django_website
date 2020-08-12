8527465c9ef406fd16a51041a571992a7a7c75e8
Rafael Dalvas <rafaeldalvas@gmail.com>
2020-08-11 22:46:32 -0300

    validacao email

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,15 @@ from django.contrib.auth.models import User
 
 class UserCreationFormWithEmail(UserCreationForm):
     email = forms.EmailField(required=True, help_text= "insira um email.")
+    first_name = forms.CharField(required=True, help_text= "insira um nome.", label="Nome")
 
     class Meta:
         model = User
-        fields =("username","email")
+        fields =("username","first_name","email")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email já cadastrado.")
+        return email
+
